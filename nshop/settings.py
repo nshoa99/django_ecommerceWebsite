062984a553a58494371e617e950a59a8258a0eeb
= <=>
2022-04-02 06:45:07 +0700

    setting

--- nshop/settings.py
+++ nshop/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['nshop-demo-django.herokuapp.com']
 
 
 # Application definition
